67ad5512d5a8c92590aad4056e976759e05220f3
HanifIHM <imamhanif2003@gmail.com>
2023-10-30 18:47:02 +0700

    edit include boolean

--- ADT/Stack/stack.c
+++ ADT/Stack/stack.c
@@ -1,7 +1,6 @@
 /* deklarasi stack yang diimplementasi dengan tabel kontigu dan ukuran sama */
 /* TOP adalah alamat elemen puncak */
 /* Implementasi dalam bahasa C dengan alokasi statik */
-#include "boolean.h"
 #include "stack.h"
 
 /* ************ Prototype ************ */
