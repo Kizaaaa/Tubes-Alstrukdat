178122176c80fb5d71fdae58b06ecf180d952a39
Amalia Putri <amaliaputriii2104@gmail.com>
2023-11-01 19:12:32 +0700

    feat(Utas)

--- ADT/Utas/utas.c
+++ ADT/Utas/utas.c
@@ -1,72 +1,42 @@
-#include "../LinkedList/listlinier.h"
-#include "../DateTime/datetime.h"
-#include "../ListDin/listdin.h"
-#include "../EntryMachine/entrymachine.h"
-#include "../Kicau/kicau.h"
-#include "../../boolean.h"
-
 #include <stdio.h>
 #include <stdlib.h>
-//UTAS linked list
-//TWEET LINKED LIST di struct nya ada pointer menuju linked list utas
+#include "../Utas/utas.h"
 
-
-/* Definisi Node : */
-typedef int ElType;
-typedef struct node* Address;
-typedef struct {
-    ElType indeks;
-    Entry text;
-    DATETIME time;
-} UTAS;
-
-typedef struct node {
-    UTAS infoutas;
-    Address next;
-} LINKEDUTAS;
-
-#define INDEKS(p) (p).indeks
-#define TEXT(p) (p).text
-#define TIME(p) (p).time
-
-#define INFOUTAS(p) (p)->infoutas
-#define NEXT(p) (p)->next
-
-
-Address newNodeLinked(UTAS val){
+Address newNodeLinked(UTAS val)
+{
     Address p;
-    p = (Address) malloc(sizeof(Node));
-    if (p!=NULL) {
+    p = (Address)malloc(sizeof(LINKEDUTAS));
+
+    if (p != NULL)
+    {
         INFOUTAS(p) = val;
         NEXT(p) = NULL;
     }
     return p;
 }
 
-typedef Address LINKEDUTAS;
+void createUtas(LINKEDUTAS *l)
+{
+    /* I.S sembarang */
+    /* F.S Terbentuk list kosong l dengan elemen bertipe utas */
 
-#define IDX_UNDEF (-1)
-#define FIRSTUTAS(p) (p)
-
-void createUtas(LINKEDUTAS* l){
     FIRSTUTAS(*l) = NULL;
 }
-/* I.S sembarang */
-/* F.S Terbentuk list kosong l dengan elemen bertipe utas */
 
-void readUtas(LINKEDUTAS l){
+void readUtas(LINKEDUTAS l)
+{
     printf("Utas berhasil dibuat!\n");
     printf("\n");
     printf("Masukkan kicauan:\n");
     Entry A;
     STARTENTRY();
-    //adt var adt entry
+    // adt var adt entry
     A = currentEntry;
-    
+
     if (CHECKVALIDTWEET(A))
     {
         // proses jika tweet valid
-        CLOSEENTRY();   
+        CLOSEENTRY();
     }
     else
     {
@@ -74,30 +44,27 @@ void readUtas(LINKEDUTAS l){
     }
     printf("Apakah Anda ingin melanjutkan kicauan ini? (YA/TIDAK)\n");
     STARTENTRY();
-
-    
 }
 /* I.S sembarang */
 /* F.S Terbentuk list kosong l dengan elemen bertipe utas */
 
-void displayUtas(LINKEDUTAS l){
+void displayUtas(LINKEDUTAS l)
+{
     Address p;
-    
-    
 }
 /* I.S sembarang */
 /* F.S Terbentuk list kosong l dengan elemen bertipe utas */
 
-void insertUtas(LINKEDUTAS* l, ElType idx, ElType id_utas, UTAS val){
+void insertUtas(LINKEDUTAS *l, ElType idx, ElType id_utas, UTAS val)
+{
     Address pointerlinked;
-
 }
 /* I.S l mungkin kosong (bukan sebuah utas) */
 /* F.S Melakukan alokasi sebuah elemen tipe bentukan UTAS dan menambahkan elemen list di akhir */
 
-void deleteUtas(LINKEDUTAS* l, ElType idx, ElType id_utas, UTAS* val){
+void deleteUtas(LINKEDUTAS *l, ElType idx, ElType id_utas, UTAS *val)
+{
     Address pointerlinked;
-    
 }
 /* I.S List l tidak kosong */
 /* F.S Melakukan penghapusan elemen bertipe UTAS  */
--- ADT/Utas/utas.h
+++ ADT/Utas/utas.h
@@ -1,68 +1,62 @@
+/* File : utas.h */
+/* PENJELASAN */
+
 #ifndef UTAS_H
 #define UTAS_H
 #include "../LinkedList/listlinier.h"
 #include "../DateTime/datetime.h"
-#include "../ListDin/listdin.h"
+// #include "../ListDin/listdin.h"
 #include "../EntryMachine/entrymachine.h"
 #include "../../boolean.h"
-//UTAS linked list
-//TWEET LINKED LIST di struct nya ada pointer menuju linked list utas
-Address newNodeLinked(UTAS val);
 
 /* Definisi Node : */
 typedef int ElType;
-typedef struct node* Address;
-typedef struct {
+typedef struct
+{
     ElType indeks;
     Entry text;
     DATETIME time;
 } UTAS;
 
-typedef struct node {
-    UTAS infoutas;
-    Address next;
-} LINKEDUTAS;
-
 #define INDEKS(p) (p).indeks
 #define TEXT(p) (p).text
 #define TIME(p) (p).time
 
+typedef struct node *Address;
+typedef struct node
+{
+    UTAS infoutas;
+    Address next;
+} LINKEDUTAS;
+
 #define INFOUTAS(p) (p)->infoutas
 #define NEXT(p) (p)->next
 
-typedef Address LINKEDUTAS;
+// UTAS linked list
+// TWEET LINKED LIST di struct nya ada pointer menuju linked list utas
+Address newNodeLinked(UTAS val);
 
 #define IDX_UNDEF (-1)
 #define FIRSTUTAS(p) (p)
 
-void createUtas(List* l){
-    FIRSTUTAS(*l) = NULL;
-}
+void createUtas(LINKEDUTAS *l);
 /* I.S sembarang */
 /* F.S Terbentuk list kosong l dengan elemen bertipe utas */
 
-void readUtas(List l){
-    
-}
+void readUtas(LINKEDUTAS l);
 /* I.S sembarang */
 /* F.S Terbentuk list kosong l dengan elemen bertipe utas */
 
-void displayUtas(List l){
-    
-}
+void displayUtas(LINKEDUTAS l);
 /* I.S sembarang */
 /* F.S Terbentuk list kosong l dengan elemen bertipe utas */
 
-void insertUtas(List* l ,ElType id_utas, ElType idx){
-    
-}
+void insertUtas(LINKEDUTAS *l, ElType id_utas, ElType idx);
 /* I.S l mungkin kosong (bukan sebuah utas) */
 /* F.S Melakukan alokasi sebuah elemen tipe bentukan UTAS dan menambahkan elemen list di akhir */
 
-void deleteUtas(List* l ,ElType id_utas, ElType idx){
-    
-}
-/* I.S List l tidak kosong */
+void deleteUtas(LINKEDUTAS *l, ElType id_utas, ElType idx);
+/* I.S list l tidak kosong */
 /* F.S Melakukan penghapusan elemen bertipe UTAS  */
 
 #endif
