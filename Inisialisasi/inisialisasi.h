6146e72d5c2f724f2978c789d15f6339b741295d
Amalia Putri <amaliaputriii2104@gmail.com>
2023-11-03 21:49:52 +0700

    feat(Inisialisasi): Config, Inisialisasi, Makefile

---
+++ Inisialisasi/inisialisasi.h
@@ -0,0 +1,10 @@
+#ifndef INISIALISASI_H
+#define INISIALISASI_H
+
+#include "../../boolean.h"
+#include "../ADT/EntryMachine/entrymachine.h"
+
+/* Melakukan inisialisasi dengan melakukan load dari file konfigurasi ketika awal menjalan program */
+void inisialisasi(Entry *namaFile);
+
+#endif
