6af44f4ac920adfd0eea222a77e549f8fbad4341
Daniel M <danielmuliaputraolo@gmail.com>
2023-11-03 19:13:05 +0800

    add listliniertweet

---
+++ ADT/ListDinLLTweet/listdintweet.h
@@ -0,0 +1,185 @@
+/* MODUL INTEGER DYNAMIC LIST */
+/* Berisi definisi dan semua primitif pemrosesan list integer */
+/* Penempatan elemen selalu rapat kiri */
+/* Versi II : dengan banyaknya elemen didefinisikan secara eksplisit,
+   memori list dinamik */
+
+#ifndef LISTDIN_H
+#define LISTDIN_H
+
+#include "../../boolean.h"
+#include "../Kicau/kicau.h"
+
+/*  Kamus Umum */
+#define IDX_MIN 0
+/* Indeks minimum list */
+#define IDX_UNDEF -1
+/* Indeks tak terdefinisi*/
+
+/* Definisi elemen dan koleksi objek */
+typedef Kicau ElType; /* type elemen list */
+typedef int IdxType;
+typedef struct
+{
+    ElType *buffer; /* memori tempat penyimpan elemen (container) */
+    int nEff;       /* >=0, banyaknya elemen efektif */
+    int capacity;   /* ukuran elemen */
+} ListDinT;
+/* Indeks yang digunakan [0..capacity-1] */
+/* Jika l adalah : ListDinT, cara deklarasi dan akses: */
+/* Deklarasi : l : ListDinT */
+/* Maka cara akses:
+   l.nEff      untuk mengetahui banyaknya elemen
+   l.buffer    untuk mengakses seluruh nilai elemen list
+   l.buffer[i] untuk mengakses elemen ke-i */
+/* Definisi :
+  list kosong: l.nEff = 0
+  Definisi elemen pertama : l.buffer[i] dengan i=0
+  Definisi elemen terakhir yang terdefinisi: l.buffer[i] dengan i=l.capacity */
+
+/* ********** SELEKTOR ********** */
+#define NEFF(l) (l).nEff
+#define BUFFER(l) (l).buffer
+#define ELMT(l, i) (l).buffer[i]
+#define CAPACITY(l) (l).capacity
+
+/* ********** KONSTRUKTOR ********** */
+/* Konstruktor : create list kosong  */
+void CreateListDinT(ListDinT *l, int capacity);
+/* I.S. l sembarang, capacity > 0 */
+/* F.S. Terbentuk list dinamis l kosong dengan kapasitas capacity */
+
+void dealocateListDinT(ListDinT *l);
+/* I.S. l terdefinisi; */
+/* F.S. (l) dikembalikan ke system, CAPACITY(l)=0; NEFF(l)=0 */
+
+/* ********** SELEKTOR (TAMBAHAN) ********** */
+/* *** Banyaknya elemen *** */
+int ListDinTLength(ListDinT l);
+/* Mengirimkan banyaknya elemen efektif list */
+/* Mengirimkan nol jika list l kosong */
+/* *** Daya tampung container *** */
+
+/* *** Selektor INDEKS *** */
+IdxType getFirstIdxListDinT(ListDinT l);
+/* Prekondisi : List l tidak kosong */
+/* Mengirimkan indeks elemen l pertama */
+IdxType getLastIdxListDinT(ListDinT l);
+/* Prekondisi : List l tidak kosong */
+/* Mengirimkan indeks elemen l terakhir */
+
+/* ********** Test Indeks yang valid ********** */
+boolean isIdxValidListDinT(ListDinT l, IdxType i);
+/* Mengirimkan true jika i adalah indeks yang valid utk kapasitas list l */
+/* yaitu antara indeks yang terdefinisi utk container*/
+boolean isIdxEffListDinT(ListDinT l, IdxType i);
+/* Mengirimkan true jika i adalah indeks yang terdefinisi utk list */
+/* yaitu antara 0..NEFF(l) */
+
+/* ********** TEST KOSONG/PENUH ********** */
+/* *** Test list kosong *** */
+boolean isEmptyListDinT(ListDinT l);
+/* Mengirimkan true jika list l kosong, mengirimkan false jika tidak */
+/* *** Test list penuh *** */
+boolean isFullListDinT(ListDinT l);
+/* Mengirimkan true jika list l penuh, mengirimkan false jika tidak */
+
+/* ********** BACA dan TULIS dengan INPUT/OUTPUT device ********** */
+/* *** Mendefinisikan isi list dari pembacaan *** */
+void readListDinT(ListDinT *l);
+/* I.S. l sembarang dan sudah dialokasikan sebelumnya */
+/* F.S. List l terdefinisi */
+/* Proses : membaca banyaknya elemen l dan mengisi nilainya */
+/* 1. Baca banyaknya elemen diakhiri enter, misalnya N */
+/*    Pembacaan diulangi sampai didapat N yang benar yaitu 0 <= N <= CAPACITY(l) */
+/*    Jika N tidak valid, tidak diberikan pesan kesalahan */
+/* 2. Jika 0 < N <= CAPACITY(l); Lakukan N kali: Baca elemen mulai dari indeks
+      0 satu per satu diakhiri enter */
+/*    Jika N = 0; hanya terbentuk l kosong */
+void printListDinT(ListDinT l);
+/* Proses : Menuliskan isi list dengan traversal, list ditulis di antara kurung siku;
+   antara dua elemen dipisahkan dengan separator "koma", tanpa tambahan karakter di depan,
+   di tengah, atau di belakang, termasuk spasi dan enter */
+/* I.S. l boleh kosong */
+/* F.S. Jika l tidak kosong: [e1,e2,...,en] */
+/* Contoh : jika ada tiga elemen bernilai 1, 20, 30 akan dicetak: [1,20,30] */
+/* Jika list kosong : menulis [] */
+
+/* ********** OPERATOR ARITMATIKA ********** */
+/* *** Aritmatika list : Penjumlahan, pengurangan, perkalian, ... *** */
+ListDinT plusMinusList(ListDinT l1, ListDinT l2, boolean plus);
+/* Prekondisi : l1 dan l2 memiliki Neff sama dan tidak kosong */
+/* Jika plus = true, mengirimkan  l1+l2, yaitu setiap elemen l1 dan l2 pada indeks yang sama dijumlahkan */
+/* Jika plus = false, mengirimkan l1-l2, yaitu setiap elemen l1 dikurangi elemen l2 pada indeks yang sama */
+
+/* ********** OPERATOR RELASIONAL ********** */
+/* *** Operasi pembandingan list : < =, > *** */
+boolean isListDinTEqual(ListDinT l1, ListDinT l2);
+/* Mengirimkan true jika l1 sama dengan l2 yaitu jika nEff l1 = l2 dan semua elemennya sama */
+
+/* ********** SEARCHING ********** */
+/* ***  Perhatian : list boleh kosong!! *** */
+IdxType indexListDinTOf(ListDinT l, ElType val);
+/* Search apakah ada elemen List l yang bernilai val */
+/* Jika ada, menghasilkan indeks i terkecil, dengan elemen ke-i = val */
+/* Jika tidak ada, mengirimkan IDX_UNDEF */
+/* Menghasilkan indeks tak terdefinisi (IDX_UNDEF) jika List l kosong */
+/* Skema Searching yang digunakan bebas */
+
+/* ********** NILAI EKSTREM ********** */
+void extremeValues(ListDinT l, ElType *max, ElType *min);
+/* I.S. List l tidak kosong */
+/* F.S. max berisi nilai maksimum l;
+        min berisi nilai minimum l */
+
+/* ********** OPERASI LAIN ********** */
+void copyListDinT(ListDinT lIn, ListDinT *lOut);
+/* I.S. lIn terdefinisi tidak kosong, lOut sembarang */
+/* F.S. lOut berisi salinan dari lIn (identik, nEff dan capacity sama) */
+/* Proses : Menyalin isi lIn ke lOut */
+ElType sumListDinT(ListDinT l);
+/* Menghasilkan hasil penjumlahan semua elemen l */
+/* Jika l kosong menghasilkan 0 */
+int countVal(ListDinT l, ElType val);
+/* Menghasilkan berapa banyak kemunculan val di l */
+/* Jika l kosong menghasilkan 0 */
+
+/* ********** SORTING ********** */
+void sortListDinT(ListDinT *l, boolean asc);
+/* I.S. l boleh kosong */
+/* F.S. Jika asc = true, l terurut membesar */
+/*      Jika asc = false, l terurut mengecil */
+/* Proses : Mengurutkan l dengan salah satu algoritma sorting,
+   algoritma bebas */
+
+/* ********** MENAMBAH DAN MENGHAPUS ELEMEN DI AKHIR ********** */
+/* *** Menambahkan elemen terakhir *** */
+void insertLastListDinT(ListDinT *l, ElType val);
+/* Proses: Menambahkan val sebagai elemen terakhir list */
+/* I.S. List l boleh kosong, tetapi tidak penuh */
+/* F.S. val adalah elemen terakhir l yang baru */
+/* ********** MENGHAPUS ELEMEN ********** */
+void deleteLastListDinT(ListDinT *l, ElType *val);
+/* Proses : Menghapus elemen terakhir list */
+/* I.S. List tidak kosong */
+/* F.S. val adalah nilai elemen terakhir l sebelum penghapusan, */
+/*      Banyaknya elemen list berkurang satu */
+/*      List l mungkin menjadi kosong */
+
+/* ********* MENGUBAH UKURAN ARRAY ********* */
+void expandListDinT(ListDinT *l, int num);
+/* Proses : Menambahkan capacity l sebanyak num */
+/* I.S. List sudah terdefinisi */
+/* F.S. Ukuran list bertambah sebanyak num */
+
+void shrinkList(ListDinT *l, int num);
+/* Proses : Mengurangi capacity sebanyak num */
+/* I.S. List sudah terdefinisi, ukuran capacity > num, dan nEff < capacity - num. */
+/* F.S. Ukuran list berkurang sebanyak num. */
+
+void compressList(ListDinT *l);
+/* Proses : Mengubah capacity sehingga capacity = nEff */
+/* I.S. List tidak kosong */
+/* F.S. Ukuran capacity = nEff */
+
+#endif
---
+++ Program/Draf/Draf.h
@@ -0,0 +1,9 @@
+#include "../../boolean.h"
+#include "../../ADT/ListDin/listdin.h"
+#include "../../ADT/EntryMachine/entrymachine.h"
+#include "../../ADT/EntryMachine/charmachine.h"
+#include "../../ADT/Kicau/kicau.h"
+#include "../../ADT/LinkedListTweet/listliniertweet.h"
+#include "../../ADT/ListDinLLTweet/listdintweet.h"
+
+void InitializeDraf(ListDinT *l);
